a6bd1a7ab00c9a7c8951216fd241f37542a41702
Jonnel Diego Quispe Reyes <u201924326@upc.edu.pe>
2022-05-31 20:29:41 -0500

    Eliminar Libros y Buscar Libros
    Implementación de codigo

--- Edudemic-Spring/src/main/java/com/edudemic/controller/VideoController.java
+++ Edudemic-Spring/src/main/java/com/edudemic/controller/VideoController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.edudemic.entities.Categoria;
@@ -53,9 +52,5 @@ public class VideoController {
 		return "redirect:/";
 	}
 	
-    @GetMapping("/videos/eliminar/{id}")
-    public String eliminarVideo(@PathVariable Long id) {
-        videoService.eliminarVideo(id);
-        return "redirect:/list/video";
-    }
+	
 }

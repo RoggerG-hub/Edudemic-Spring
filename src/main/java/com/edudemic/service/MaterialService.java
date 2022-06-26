a884de9885699b39f0602ee8e3405dc72ba69ee0
luis-ux107 <luisleon7677@gmail.com>
2022-06-26 14:18:17 -0500

    modificacion de enlaces y materiales academicos para estudiantes

--- src/main/java/com/edudemic/service/MaterialService.java
+++ src/main/java/com/edudemic/service/MaterialService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.edudemic.entities.Libro;
 import com.edudemic.entities.Material;
 import com.edudemic.repository.MaterialRepository;
 
@@ -25,7 +26,17 @@ public class MaterialService {
 	public Material materialObjeto(long id) {
 		return materialRepository.objetoM(id);
 	}
-	
+	// modificar
+		public Material updateMaterial(Material material) {
+			return materialRepository.save(material);
+		}
+
+		public Material getMaterialById(Long id) {
+			return materialRepository.findById(id).get();
+		}
+
+		// eliminar
+		
 	public List<Material>listarMaterialesMentoria(Long id){
 		return materialRepository.listaMaterialesMentoria(id);
 	}
